6b925d678a3e6ae770d7bcd63086b41a44698d3e
Tymon Becella <tymon@becella.eu>
2021-10-10 17:20:13 +0200

    init

---
+++ combinations.py
@@ -0,0 +1,25 @@
+#0-up
+#1-left
+#2-down
+#3-right
+
+def AppendDirs(array):
+    set = []
+    for listIndex in range(len(array)):
+        for direction in range(4):
+            set.append(array[listIndex].copy())
+            set[direction+4*listIndex].append(direction)
+    return set
+
+
+def GenerateCombinations(depth):
+    combinations = [[]]
+    for depth in range(depth):
+        combinations = AppendDirs(combinations)
+    return combinations
+
+def main():
+    print(GenerateCombinations(5))
+
+if __name__ == '__main__':
+    main()
